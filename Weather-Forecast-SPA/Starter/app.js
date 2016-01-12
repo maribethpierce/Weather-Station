43da7fea42b9e40fef3968e7ba0bdf685e67a937
Maribeth <maribethpierce@gmail.com>
2016-01-12 13:43:54 -0500

    directives tidied up

--- Weather-Forecast-SPA/Starter/app.js
+++ Weather-Forecast-SPA/Starter/app.js
@@ -68,3 +68,19 @@ weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParam
     console.log($scope.weatherResult);
     
 }]);
+
+//DIRECTIVES
+weatherApp.directive("weatherReport", function() {
+   return {
+       restrict: "E",
+       templateUrl: 'directives/weatherReport.html',
+       replace: true,
+       scope: {
+           weatherDay: "=",
+           convertToStandard: "&",
+           convertToDate: "&",
+           dateFormat: "@"
+       }
+       
+   } 
+});
