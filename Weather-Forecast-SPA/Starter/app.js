c4cef81259ec2408abae33e5426f5262a23e0cd2
Maribeth <maribethpierce@gmail.com>
2016-01-12 13:15:28 -0500

    touchups

--- Weather-Forecast-SPA/Starter/app.js
+++ Weather-Forecast-SPA/Starter/app.js
@@ -16,6 +16,11 @@ weatherApp.config(function ($routeProvider) {
         controller: 'forecastController'
     })
     
+    .when('/forecast/:days', {
+      templateUrl: 'pages/forecast.htm',
+        controller: 'forecastController'
+    })
+    
 });
 
 // SERVICES
@@ -36,17 +41,19 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
     
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService', function($scope, $resource, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
     
     $scope.city = cityService.city;
     
     $scope.weatherAPPID = "8a24faef01c610b877fa16b07a920c26"
     
+    $scope.days = $routeParams.days || '2';
+    
     $scope.convertToFahrenheit = function(degK) {
         
         return Math.round((1.8 * (degK - 273)) + 32);
         
-    }
+    };
     
     $scope.convertToDate = function(dt) {
         
@@ -56,7 +63,7 @@ weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService
     
     $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", { callback: "JSON_CALLBACK"}, {get: { method: "JSONP"}});
     
-    $scope.weatherResult = $scope.weatherAPI.get( { q: $scope.city, cnt: 2, appid: $scope.weatherAPPID });
+    $scope.weatherResult = $scope.weatherAPI.get( { q: $scope.city, cnt: $scope.days, appid: $scope.weatherAPPID });
     
     console.log($scope.weatherResult);
     
