43c665fba6399af5c86c673a64749c98564d74b7
Maribeth <maribethpierce@gmail.com>
2016-01-12 12:37:52 -0500

    app is coming along

--- Weather-Forecast-SPA/Starter/app.js
+++ Weather-Forecast-SPA/Starter/app.js
@@ -1,2 +1,63 @@
 // MODULE
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+//ROUTES
+weatherApp.config(function ($routeProvider) {
+    
+    $routeProvider
+    
+    .when('/', {
+        templateUrl: 'pages/home.htm',
+        controller: 'homeController'
+    })
+    
+    .when('/forecast', {
+        templateUrl: 'pages/forecast.htm',
+        controller: 'forecastController'
+    })
+    
+});
+
+// SERVICES
+weatherApp.service('cityService', function() {
+    
+    this.city = "New York, NY";
+    
+})
+
+//CONTROLLERS
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+    
+    $scope.city = cityService.city;
+    
+    $scope.$watch('city', function() {
+        cityService.city = $scope.city;
+    });
+    
+}]);
+
+weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService', function($scope, $resource, cityService) {
+    
+    $scope.city = cityService.city;
+    
+    $scope.weatherAPPID = "8a24faef01c610b877fa16b07a920c26"
+    
+    $scope.convertToFahrenheit = function(degK) {
+        
+        return Math.round((1.8 * (degK - 273)) + 32);
+        
+    }
+    
+    $scope.convertToDate = function(dt) {
+        
+        return new Date(dt * 1000);
+        
+    }
+    
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", { callback: "JSON_CALLBACK"}, {get: { method: "JSONP"}});
+    
+    $scope.weatherResult = $scope.weatherAPI.get( { q: $scope.city, cnt: 2, appid: $scope.weatherAPPID });
+    
+    console.log($scope.weatherResult);
+    
+}]);
